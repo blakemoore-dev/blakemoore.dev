59d4a801d0a4240322439c137667845dce391ed1
blakemoore-dev <lostinthestack@protonmail.com>
2021-10-13 09:01:23 -0500

    purge

---
+++ src/components/layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+export default function Layout({pageTitle, children}) {
+    return (
+        <div>
+            <title>{pageTitle}</title>
+        </div>
+    )
+}
